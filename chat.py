1686e639950eb63f01cbf4c7e4ae5fe820f835bd
Emil Ahlbäck <emil.ahlback99@gmail.com>
2023-03-08 19:29:13 +0100

    remove msg hist

--- chat.py
+++ chat.py
@@ -1,18 +1,21 @@
 import openai
 
-messagehistory = [
+preprompts = [
     {"role": "user", "content": "You are a helpful assistant called Sherlock. Your personality is vaguely based off of Sherlock Holmes."},
 ]
+messagehistory = []
 
 def add_history(text, role="user"):
     messagehistory.append({"role": role, "content": text})
+    if len(messagehistory) > 6:
+        messagehistory = messagehistory[-6:]
 
 
 async def chat(text):
     add_history(text)
     ans = await openai.ChatCompletion.acreate(
         model="gpt-3.5-turbo",
-        messages=messagehistory,
+        messages=preprompts + messagehistory,
     )
     m = ans.choices[0].message
     messagehistory.append(m)
